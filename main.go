4c596df10f64697dbb9e59cf6fb4e711352f7add
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-04-04 22:08:52 +0700

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"challenge-2/database"
+	"challenge-2/router"
+)
+
+func main() {
+	database.StartDB()
+	r := router.StartApp()
+	r.Run(":8081")
+}
---
+++ router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"challenge-2/controllers"
+	"challenge-2/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartApp() *gin.Engine {
+	r := gin.Default()
+
+	userRouter := r.Group("/users")
+	{
+		userRouter.POST("/register", controllers.UserRegister)
+		userRouter.POST("/login", controllers.UserLogin)
+	}
+
+	productRouter := r.Group("/products")
+	{
+		productRouter.Use(middlewares.Authentication())
+		productRouter.POST("/", controllers.CreateProduct)
+
+		productRouter.PUT("/:productId", middlewares.ProductAuthorization(), controllers.UpdateProduct)
+	}
+	return r
+}
