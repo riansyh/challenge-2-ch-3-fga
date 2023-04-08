ef319a39d66967e663ac8f70dbe6f15fce3960e6
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-04-08 09:56:32 +0700

    feat: change port using env

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"challenge-2/database"
 	"challenge-2/router"
+	"os"
 )
 
 func main() {
 	database.StartDB()
 	r := router.StartApp()
-	r.Run(":8081")
+	var PORT = os.Getenv("PORT")
+	// r.Run(":8081")
+	r.Run(":" + PORT)
+
 }
